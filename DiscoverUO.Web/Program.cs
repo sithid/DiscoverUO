485bad341a4075d947e01d50e8aaf739f93537b1
sithid <demonic_urges05@hotmail.com>
2024-11-10 20:39:31 -0500

    Blazor Project Changes
    Changes to the blazor project (DiscoverUO.Web blazor frontend).

--- DiscoverUO.Web/Program.cs
+++ DiscoverUO.Web/Program.cs
@@ -1,43 +1,19 @@
-using DiscoverUO.Web.Components;
-using DiscoverUO.Web.Services.Authentication;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace DiscoverUO.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddRazorComponents();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri("http://localhost:7015/"),
-            });
-
-            builder.Services.AddTransient<AuthenticationService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>();
-
-            app.Run();
+            await builder.Build().RunAsync();
         }
     }
 }
